e018ecd5344a7dbdce0d4e1a6df4aedae5c4247c
JessicaFerreiraLopez <jessicadaniela.ferreiralopez@gmail.com>
2023-05-13 00:24:42 -0300

    added prettier and basic Tailwind configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p>Jessica Ferreira challenge</p>
-    </div>
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
   );
 }
 
